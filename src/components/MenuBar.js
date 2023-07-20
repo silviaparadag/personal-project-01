9d9bd07c04139064b0cd190a436a2433ea91c2d7
Silvia Parada <silviaparadag@yahoo.es>
2023-07-20 19:41:51 +0200

    Set slide menu bar with react module

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,45 +1,53 @@
-
+import { slide as Menu } from 'react-burger-menu';
 import '../styles/layout/MenuBar.scss';
+import { useState } from 'react';
 
 const MenuBar = () => {
-
+  const [menuOpenState, setMenuOpenState] = useState(false);
  
+  const handleMenuBar = () => {
+    if (menuOpenState === false) {
+      return setMenuOpenState(true);
+    } else {
+      setMenuOpenState(false);
+    }
+  };
+
   return (
-    <>
-      <section className='menu hidden'>
-        <a  className="menu__text" href="/#" alt="Inicio">
+      <Menu isOpen={ menuOpenState }>
+        <a  className="menu__text" href="/#" alt="Inicio" onClick={handleMenuBar}>
           Home
         </a>
         <a className="menu__text"  href="/#" alt="Cities">
           Cities
         </a>
-
+        <div>
           <ul className="menu__sm">
-            <li>
-              <a
-                className="menu__sm--icon"
-                href="https://github.com/silviaparadag"
-                alt="Silvia Paradas's Github"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i className="fa-brands fa-github-alt" />
-              </a>
-            </li>
-            <li>
-              <a
-                className="menu__sm--icon"
-                href="https://www.linkedin.com/in/silvia-parada-905b246/"
-                alt="Silvia's LinkedIn"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i className="fa-brands fa-linkedin-in"></i>
-              </a>
-            </li>
-          </ul>
-      </section>
-    </>
+              <li>
+                <a
+                  className="menu__sm--icon"
+                  href="https://github.com/silviaparadag"
+                  alt="Silvia Paradas's Github"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fa-brands fa-github-alt" />
+                </a>
+              </li>
+              <li>
+                <a
+                  className="menu__sm--icon"
+                  href="https://www.linkedin.com/in/silvia-parada-905b246/"
+                  alt="Silvia's LinkedIn"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fa-brands fa-linkedin-in"></i>
+                </a>
+              </li>
+            </ul>
+        </div>
+      </Menu>
   );
 };
 export default MenuBar;
